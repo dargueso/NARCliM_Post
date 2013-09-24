86a9c36e140caa243590b37365fe4513bef866f4
Alejandro Di Luca <z3444417@monsoon.ccrc.unsw.edu.au>
2013-09-24 16:09:18 +1000

    NARCliMPost: I modified the computation of the number of files (n_files) for the wrfout case...

--- postprocess_NARCliM.py
+++ postprocess_NARCliM.py
@@ -53,7 +53,7 @@ fullpathout=pm.create_outdir(gvars)
 datenow=dt.datetime.now().strftime("%Y-%m-%d_%H:%M")
 logfile = '%spostprocess_%s_%s_%s-%s_%s_%s.log' %(fullpathout,gvars.GCM,gvars.RCM,gvars.syear,gvars.eyear,gvars.domain,datenow)
 print 'The output messages are written to %s' %(logfile)
-sys.stdout = open('%s' %(logfile), "w") 
+#sys.stdout = open('%s' %(logfile), "w") 
 
 #***********************************************
 # LOOP OVER ALL TYPES OF WRF FILE OUTPUTS (i.e., wrfhrly, wrfout, etc) 
@@ -101,10 +101,11 @@ for filet in file_type:
 		for pp in np.arange(per,per_f+1):
 			if cal.isleap(pp):
 				n_leap=n_leap+1	
-			if filet=='wrfout' and gvars.GCM_calendar!='no_leap':
-				n_files=perstep*365+n_leap
-			else:
-				n_files=perstep*365
+			if filet=='wrfout':
+				if gvars.GCM_calendar!='no_leap':
+					n_files=perstep*365+n_leap
+				else:
+					n_files=perstep*365
 				
 
 		# SELECTING FILES TO READ
