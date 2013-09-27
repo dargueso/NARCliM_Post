c85011c238b0b310eb50d6b934602a81c99e0d4b
Daniel Argüeso <d.argueso@unsw.edu.au>
2013-09-27 11:38:20 +1000

    NARCliMPost: Added checking at the end. Indentation is now consistent with Alejandro's one

--- compute_stats.py
+++ compute_stats.py
@@ -9,84 +9,84 @@ import datetime as dt
 import sys
 import postprocess_modules as pm
 def compute_daily(var,time,stat):
-	"""Method to compute daily statistics
-	   var: variable that will be processed
-	   time: list of times corresponding to var 1st dimension
-	   stat: requested stat (now 'acc', 'mean', 'min' or 'max')
-	   ---
-	   dvar: daily statistic of the variable
-	   dtime: list of times corresponding to dailyvar 1st dimension
-	"""
-	if stat not in ['acc', 'mean','min','max']:
-		sys.exit("ERROR the requested daily statistic %s does not exist. Please choose between 'acc', 'mean', 'min' or 'max'" %(stat))
-	if len(time)!=var.shape[0]:
-	    sys.exit('ERROR in compute_daily: The lenght of time variable does not correspond to var first dimension')
-	#Remove hours from time list, keep only dates
-	ndays=(time[-1]-time[0]).days+1
-	sdate=time[0].date()
-	stime=dt.datetime(sdate.year,sdate.month,sdate.day,12,0,0)
-	dtime=[stime+dt.timedelta(days=x) for x in xrange(ndays)]
-	nsteps=len(time)/ndays
-	#Remove degenerated dimensions
-	var=np.squeeze(var)
-	#In the reshape, keep right order for the last two dimensions (lat,lon)
-	var_r=np.reshape(var,[nsteps,ndays,var.shape[-2],var.shape[-1]],order='F')
-	if stat == 'acc':
-	    dvar=np.ma.sum(var_r,axis=0)
-	elif stat == 'mean':
-	    dvar=np.ma.mean(var_r,axis=0)
-	elif stat == 'max':
-	    dvar=np.ma.max(var_r,axis=0)
-	elif stat == 'min':
-	    dvar=np.ma.min(var_r,axis=0)
+  """Method to compute daily statistics
+     var: variable that will be processed
+     time: list of times corresponding to var 1st dimension
+     stat: requested stat (now 'acc', 'mean', 'min' or 'max')
+     ---
+     dvar: daily statistic of the variable
+     dtime: list of times corresponding to dailyvar 1st dimension
+  """
+  if stat not in ['acc', 'mean','min','max']:
+    sys.exit("ERROR the requested daily statistic %s does not exist. Please choose between 'acc', 'mean', 'min' or 'max'" %(stat))
+  if len(time)!=var.shape[0]:
+      sys.exit('ERROR in compute_daily: The lenght of time variable does not correspond to var first dimension')
+  #Remove hours from time list, keep only dates
+  ndays=(time[-1]-time[0]).days+1
+  sdate=time[0].date()
+  stime=dt.datetime(sdate.year,sdate.month,sdate.day,12,0,0)
+  dtime=[stime+dt.timedelta(days=x) for x in xrange(ndays)]
+  nsteps=len(time)/ndays
+  #Remove degenerated dimensions
+  var=np.squeeze(var)
+  #In the reshape, keep right order for the last two dimensions (lat,lon)
+  var_r=np.reshape(var,[nsteps,ndays,var.shape[-2],var.shape[-1]],order='F')
+  if stat == 'acc':
+      dvar=np.ma.sum(var_r,axis=0)
+  elif stat == 'mean':
+      dvar=np.ma.mean(var_r,axis=0)
+  elif stat == 'max':
+      dvar=np.ma.max(var_r,axis=0)
+  elif stat == 'min':
+      dvar=np.ma.min(var_r,axis=0)
 
-	return dvar,dtime
+  return dvar,dtime
     
 def compute_monthly(var,time,stat):
-	"""Method to compute monthly statistics
-	   var: variable that will be processed
-	   time: list of times corresponding to var 1st dimension
-	   stat: requested stat (now 'acc', 'mean', 'min' or 'max')
-	   ---
-	   mvar: monthly statistic of the variable
-	   mtime: list of times corresponding to mvar 1st dimension
-	"""
-	if stat not in ['acc', 'mean','min','max']:
-		sys.exit("ERROR the requested monthly statistic %s does not exist. Please choose between 'acc', 'mean', 'min' or 'max'" %(stat))
-	if len(time)!=var.shape[0]:
-		sys.exit('ERROR in compute_monthly: The lenght of time variable does not correspond to var first dimension')
-	#Remove hours from time list, keep only dates
-	years=np.asarray([time[i].year for i in xrange(len(time))])-time[0].year
-	climmonths=np.asarray([time[i].month for i in xrange(len(time))])
-	months=years*12+climmonths
-	
+  """Method to compute monthly statistics
+     var: variable that will be processed
+     time: list of times corresponding to var 1st dimension
+     stat: requested stat (now 'acc', 'mean', 'min' or 'max')
+     ---
+     mvar: monthly statistic of the variable
+     mtime: list of times corresponding to mvar 1st dimension
+  """
+  if stat not in ['acc', 'mean','min','max']:
+    sys.exit("ERROR the requested monthly statistic %s does not exist. Please choose between 'acc', 'mean', 'min' or 'max'" %(stat))
+  if len(time)!=var.shape[0]:
+    sys.exit('ERROR in compute_monthly: The lenght of time variable does not correspond to var first dimension')
+  #Remove hours from time list, keep only dates
+  years=np.asarray([time[i].year for i in xrange(len(time))])-time[0].year
+  climmonths=np.asarray([time[i].month for i in xrange(len(time))])
+  months=years*12+climmonths
+  
 
-	var=np.squeeze(var)
-	mvar=np.ma.ones((max(months),)+var.shape[1:],dtype=np.float64)*pm.const.missingval
+  var=np.squeeze(var)
+  mvar=np.ma.ones((max(months),)+var.shape[1:],dtype=np.float64)*pm.const.missingval
 
-	#Calculating the middle of each month. Data is provided in the mid point between the time_bounds
-	mtime=[0]*max(months)
-	for mo in xrange(max(months)):
-		time_m=time[months==mo+1]
-		tdiference=(time_m[-1]-time_m[0]).total_seconds()/2
-		mtime[mo]=time_m[0]+dt.timedelta(seconds=tdiference)
-
-
-	if stat == 'acc':
-	    for mo in xrange(max(months)):
-	        mvar[mo,:,:]=np.ma.sum(var[months==mo+1,:,:],axis=0)
-	if stat == 'mean':
-	    for mo in xrange(max(months)):
-	        mvar[mo,:,:]=np.ma.mean(var[months==mo+1,:,:],axis=0)
-	if stat == 'max':
-	    for mo in xrange(max(months)):
-	        mvar[mo,:,:]=np.ma.max(var[months==mo+1,:,:],axis=0)
-	if stat == 'min':
-	    for mo in xrange(max(months)):
-	        mvar[mo,:,:]=np.ma.min(var[months==mo+1,:,:],axis=0)
+  #Calculating the middle of each month. Data is provided in the mid point between the time_bounds
+  mtime=[0]*max(months)
+  for mo in xrange(max(months)):
+    time_m=time[months==mo+1]
+    tdiference=(time_m[-1]-time_m[0]).total_seconds()/2
+    mtime[mo]=time_m[0]+dt.timedelta(seconds=tdiference)
 
 
-	return mvar,mtime
+  if stat == 'acc':
+    for mo in xrange(max(months)):
+          mvar[mo,:,:]=np.ma.sum(var[months==mo+1,:,:],axis=0)
+  if stat == 'mean':
+      for mo in xrange(max(months)):
+          mvar[mo,:,:]=np.ma.mean(var[months==mo+1,:,:],axis=0)
+  if stat == 'max':
+      for mo in xrange(max(months)):
+          mvar[mo,:,:]=np.ma.max(var[months==mo+1,:,:],axis=0)
+  if stat == 'min':
+      for mo in xrange(max(months)):
+          mvar[mo,:,:]=np.ma.min(var[months==mo+1,:,:],axis=0)
+
+
+  return mvar,mtime
 
 
             
--- postprocess_NARCliM.py
+++ postprocess_NARCliM.py
@@ -48,6 +48,7 @@ file_type=varinfo.keys()
 #### Creating global variables ####
 gvars=pm.gvar(inputinf)
 fullpathout=pm.create_outdir(gvars)
+
  
 #CREATE A LOG IFLE TO PUT OUTPUT FROM THE MAIN SCRIPT
 datenow=dt.datetime.now().strftime("%Y-%m-%d_%H:%M")
@@ -58,140 +59,188 @@ print 'The output messages are written to %s' %(logfile)
 #***********************************************
 # LOOP OVER ALL TYPES OF WRF FILE OUTPUTS (i.e., wrfhrly, wrfout, etc) 
 for filet in file_type:
-	ctime_filet=pm.checkpoint(0)
-	print '\n','\n', '*************************************'
-	print '  PROCESSING ', filet, ' FILE OUTPUTS'
-	print '*************************************'
-	sper=gvars.syear
-	eper=gvars.eyear
-	perstep=1
+  ctime_filet=pm.checkpoint(0)
+  print '\n','\n', '*************************************'
+  print '  PROCESSING ', filet, ' FILE OUTPUTS'
+  print '*************************************'
+  sper=gvars.syear
+  eper=gvars.eyear
 
-	if filet=='wrfhrly':
-		n_files=12      
-		time_step=1 #hours between two time steps
-		file_freq='01H'
-		tbounds=False
-	
-	if filet=='wrfout':
-		time_step=3 #hours between two time steps
-		file_freq='03H'
-		tbounds=False
-		perstep=5
+  # Getting information about the input file type
+  n_files,time_step,file_freq,tbounds,perstep=pm.get_filefreq(filet)
 
-	if filet=='wrfxtrm':
-		n_files=60      
-		time_step=24 #hours between two time steps
-		file_freq='DAY'
-		tbounds=True
-		perstep=5
 
-	if filet=='wrfdly':
-		n_files=60      
-		time_step=24 #hours between two time steps
-		file_freq='DAY'
-		tbounds=True
-		perstep=5
+  # LOOP OVER PERIODS
+  for per in np.arange(sper,eper+1,perstep):
+    ctime_year=pm.checkpoint(0)
+    per_f=per+perstep-1
 
-        # LOOP OVER PERIODS
-	for per in np.arange(sper,eper+1,perstep):
-		ctime_year=pm.checkpoint(0)
-		per_f=per+perstep-1
+    n_leap=0
+    for pp in np.arange(per,per_f+1):
+      if cal.isleap(pp):
+        n_leap=n_leap+1	
+      if filet=='wrfout':
+        if gvars.GCM_calendar!='no_leap':
+          n_files=perstep*365+n_leap
+        else:
+          n_files=perstep*365
 
-		n_leap=0
-		for pp in np.arange(per,per_f+1):
-			if cal.isleap(pp):
-				n_leap=n_leap+1	
-			if filet=='wrfout':
-				if gvars.GCM_calendar!='no_leap':
-					n_files=perstep*365+n_leap
-				else:
-					n_files=perstep*365
-				
+    # SELECTING FILES TO READ
+    files_list=pm.file_list(gvars, per, per_f, filet, n_files)
 
-		# SELECTING FILES TO READ
-		files_list=pm.file_list(gvars, per, per_f, filet, n_files)
+    # LOOP OVER VARIABLES IN THE GIVEN KIND OF FILE
+    for var in pm.intersect(varinfo[filet].keys(),out_variables):
+      ctime_var=pm.checkpoint(0)
 
-		# LOOP OVER VARIABLES IN THE GIVEN KIND OF FILE
-		for var in pm.intersect(varinfo[filet].keys(),out_variables):
-			ctime_var=pm.checkpoint(0)
-			
-			# CHECK IF THE FILE ALREADY EXISTS
-			file_out='%s%s%s_%s-%s_%s.nc' % (fullpathout,gvars.outfile_patt,file_freq,per,per_f,var) # Specify output file
-			filewrite=pm.checkfile(file_out,gvars.overwrite)
-			if filewrite==True:
-				
-				# READ FILES FROM THE CORRESPONDING PERIOD
-				wrfvar=(pm.getwrfname(var)[0]).split('-')
-				time_old, varvals=pm.read_list(files_list, var)	
+      # CHECK IF THE FILE ALREADY EXISTS
+      file_out='%s%s%s_%s-%s_%s.nc' % (fullpathout,gvars.outfile_patt,file_freq,per,per_f,var) # Specify output file
+      filewrite=pm.checkfile(file_out,gvars.overwrite)
+      if filewrite==True:
 
-				# FIRST/LAST YEAR, MONTH, DAY AND HOUR OF ALL READ FILES
-				year_i, month_i, day_i, hour_i = pm.get_wrfdate(time_old[0,:])
-				year_f, month_f, day_f, hour_f = pm.get_wrfdate(time_old[-1,:])
+        # READ FILES FROM THE CORRESPONDING PERIOD
+        wrfvar=(pm.getwrfname(var)[0]).split('-')
+        time_old, varvals=pm.read_list(files_list, var)	
 
-				# DEFINE TIME BOUNDS VARIABLES 
-				time_bounds=tbounds
-				time_bnds=pm.const.missingval
+        # FIRST/LAST YEAR, MONTH, DAY AND HOUR OF ALL READ FILES
+        year_i, month_i, day_i, hour_i = pm.get_wrfdate(time_old[0,:])
+        year_f, month_f, day_f, hour_f = pm.get_wrfdate(time_old[-1,:])
 
-				# DEFINE DATES USING STANDARD CALENDAR
-				n_days = dt.datetime(per_f+1,month_i,day_i,hour_i)-dt.datetime(per,month_i,day_i,hour_i)
-				n_timesteps=n_days.days*int(24./time_step)
-				date = pm.get_dates(year_i,month_i,day_i,hour_i,0,time_step,n_timesteps)
-				time=pm.date2hours(date,gvars.ref_date)
+        # DEFINE TIME BOUNDS VARIABLES 
+        time_bounds=tbounds
+        time_bnds=pm.const.missingval
 
-				# ADD LEAP DAY FOR MODELS WITHOU IT 
-				if gvars.GCM_calendar=='no_leap' and n_leap>=1:
-					varvals=pm.add_leapdays(varvals,wrfvar,date,time_step)
+        # DEFINE DATES USING STANDARD CALENDAR
+        n_days = dt.datetime(per_f+1,month_i,day_i,hour_i)-dt.datetime(per,month_i,day_i,hour_i)
+        n_timesteps=n_days.days*int(24./time_step)
+        date = pm.get_dates(year_i,month_i,day_i,hour_i,0,time_step,n_timesteps)
+        time=pm.date2hours(date,gvars.ref_date)
 
-				# ***********************************************
-				# ACCUMULATED VARIABLES NEED ONE TIME STEP MORE TO COMPUTE DIFFERENCES
-				if var=='pracc' or var=='potevp' or var=='evspsbl':
+        # ADD LEAP DAY FOR MODELS WITHOU IT 
+        if gvars.GCM_calendar=='no_leap' and n_leap>=1:
+          varvals=pm.add_leapdays(varvals,wrfvar,date,time_step)
 
-					# DEFINE TIME BOUNDS FOR ACCUMULATED VARIABLES
-					time_bounds=True
-					if filet=='wrfhrly' or filet=='wrfout':
-						time=pm.create_outtime(date,gvars)
-						time_bnds=pm.create_timebnds(time)
-						varvals=pm.add_timestep_acc(wrfvar,varvals,per_f,gvars,filet)
+        # ***********************************************
+        # ACCUMULATED VARIABLES NEED ONE TIME STEP MORE TO COMPUTE DIFFERENCES
+        if var=='pracc' or var=='potevp' or var=='evspsbl':
 
-				# ***********************************************
-				# DEFINE TIME BOUNDS FOR XTRM AND DAILY VARIABLES
-				if filet=='wrfxtrm' or filet=='wrfdly':
-					time=pm.date2hours(date,gvars.ref_date)
-					time=[time[i]+time_step/2 for i in xrange(len(time))]
-					time_bnds=pm.create_timebnds(time)
-					varvals=pm.mv_timestep(wrfvar,varvals,per_f,gvars,filet)
+          # DEFINE TIME BOUNDS FOR ACCUMULATED VARIABLES
+          time_bounds=True
+          if filet=='wrfhrly' or filet=='wrfout':
+            time=pm.create_outtime(date,gvars)
+            time_bnds=pm.create_timebnds(time)
+            varvals=pm.add_timestep_acc(wrfvar,varvals,per_f,gvars,filet)
 
-				# CALL COMPUTE_VAR MODULE
-				compute=getattr(comv,'compute_'+var) # FROM STRING TO ATTRIBUTE
-				varval, varatt=compute(varvals,date,gvars)
+        # ***********************************************
+        # DEFINE TIME BOUNDS FOR XTRM AND DAILY VARIABLES
+        if filet=='wrfxtrm' or filet=='wrfdly':
+          time=pm.date2hours(date,gvars.ref_date)
+          time=[time[i]+time_step/2 for i in xrange(len(time))]
+          time_bnds=pm.create_timebnds(time)
+          varvals=pm.mv_timestep(wrfvar,varvals,per_f,gvars,filet)
 
-				# INFO NEEDED TO WRITE THE OUTPUT NETCDF
-				netcdf_info=[file_out, var, varatt, time_bounds]
+        # CALL COMPUTE_VAR MODULE
+        compute=getattr(comv,'compute_'+var) # FROM STRING TO ATTRIBUTE
+        varval, varatt=compute(varvals,date,gvars)
 
-				# CREATE NETCDF FILE
-				pm.create_netcdf(netcdf_info, gvars, varval, time, time_bnds)
-				ctime=pm.checkpoint(ctime_var)
-				print '=====================================================', '\n', '\n', '\n'
-			print ' =======================  PERIOD: ',per, ' - ', per_f, ' FINISHED ==============', '\n', '\n',
-			ctime=pm.checkpoint(ctime_year)
-	print ' =======================  FILE TYPE :',filet, ' FINISHED ==============', '\n', '\n',
-	ctime=pm.checkpoint(ctime_filet)
+        # INFO NEEDED TO WRITE THE OUTPUT NETCDF
+        netcdf_info=[file_out, var, varatt, time_bounds]
+
+        # CREATE NETCDF FILE
+        pm.create_netcdf(netcdf_info, gvars, varval, time, time_bnds)
+        ctime=pm.checkpoint(ctime_var)
+        print '=====================================================', '\n', '\n', '\n'
+      print ' =======================  PERIOD: ',per, ' - ', per_f, ' FINISHED ==============', '\n', '\n',
+      ctime=pm.checkpoint(ctime_year)
+  print ' =======================  FILE TYPE :',filet, ' FINISHED ==============', '\n', '\n',
+  ctime=pm.checkpoint(ctime_filet)
+
+# #***********************************************
+# # DAILY STATISTICS
+# # Loop over all types of WRF output files (i.e., wrfhrly, wrfout, etc) 
+# for filet in file_type:
+#   if (filet!='wrfxtrm') and (filet!='wrfdly'): # These files are already daily
+#     for varname in varinfo[filet].keys():
+#       if varname in out_variables:
+#         stat_all=varinfo[filet][varname].split(',')
+#         pm.create_dailyfiles(gvars,varname,stat_all)
+#           
+#           
+# #***********************************************
+# # MONTHLY STATISTICS
+# for filet in file_type:
+#   for varname in varinfo[filet].keys():
+#     if varname in out_variables:
+#       stat_all=varinfo[filet][varname].split(',')
+#       pm.create_monthlyfiles(gvars,varname,stat_all)
+
 
 #***********************************************
-# DAILY STATISTICS
-# Loop over all types of WRF output files (i.e., wrfhrly, wrfout, etc) 
+# CHECKING COMPLETENESS OF ALL FILES
+badfiles=0
+badfilesname=[]
 for filet in file_type:
-	if (filet!='wrfxtrm') and (filet!='wrfdly'): # These files are already daily
-		for varname in varinfo[filet].keys():
-			if varname in out_variables:
-				stat_all=varinfo[filet][varname].split(',')
-				pm.create_dailyfiles(gvars,varname,stat_all)
-					
-					
-#***********************************************
-# MONTHLY STATISTICS
-for filet in file_type:
-	for varname in varinfo[filet].keys():
-		if varname in out_variables:
-			stat_all=varinfo[filet][varname].split(',')
-			pm.create_monthlyfiles(gvars,varname,stat_all)
+  for varname in varinfo[filet].keys():
+    if varname in out_variables:
+      print 'Checking %s files' %(varname)
+
+      n_files,time_step,file_freq,tbounds,perstep=pm.get_filefreq(filet)
+      #Checking higher-frequency ones:
+      filesvar=sorted(glob.glob('%s/%s%s*%s*.nc' %(fullpathout,gvars.outfile_patt,file_freq,varname)))
+      for filep in filesvar:
+        syfile=np.int(filep.split('%s%s_' %(gvars.outfile_patt,file_freq))[1][0:4])
+        eyfile=np.int(filep.split('%s%s_' %(gvars.outfile_patt,file_freq))[1][5:9])
+        ndays=int(np.ceil((dt.datetime(eyfile,12,31,18,0,0)-dt.datetime(syfile,1,1,0,0,0)).days))+1
+        n_steps_th=ndays*24/time_step
+        postfile=nc.Dataset(filep,'r')
+        time=postfile.variables['time']
+        n_steps_th=ndays*24/time_step
+        n_steps=len(time)
+        if n_steps==n_steps_th:
+          print '%s has the right number of time steps: %s' %(filep,n_steps)
+        else:
+          print 'WARNING: %s has not the correct number of time steps. It has %s and should have %s' %(filep, n_steps,n_steps_th)
+          badfiles=badfiles+1
+          badfilesname.append(filep)
+
+      if filet=='wrfhrly' or filet=='wrfout':
+        filesvarday=sorted(glob.glob('%s/%sDAY*%s*.nc' %(fullpathout,gvars.outfile_patt,varname)))
+        for filep in filesvarday:
+          syfile=np.int(filep.split('%sDAY_' %(gvars.outfile_patt))[1][0:4])
+          eyfile=np.int(filep.split('%sDAY_' %(gvars.outfile_patt))[1][5:9])
+          ndays=int(np.ceil((dt.datetime(eyfile,12,31,18,0,0)-dt.datetime(syfile,1,1,0,0,0)).days))+1
+          postfile=nc.Dataset(filep,'r')
+          time=postfile.variables['time'][:]
+          n_steps=len(time)
+          if n_steps==ndays:
+            print '%s has the right number of time steps: %s' %(filep,n_steps)
+          else:
+            print 'WARNING: %s has not the correct number of time steps. It has %s and should have %s' %(filep, n_steps,ndays)
+            badfiles=badfiles+1
+            badfilesname.append(filep)
+      
+      filesvarmon=sorted(glob.glob('%s/%sMON*%s*.nc' %(fullpathout,gvars.outfile_patt,varname)))
+      for filep in filesvarmon:
+        syfile=np.int(filep.split('%sMON_' %(gvars.outfile_patt))[1][0:4])
+        eyfile=np.int(filep.split('%sMON_' %(gvars.outfile_patt))[1][5:9])
+        nmon=(eyfile-syfile+1)*12
+        postfile=nc.Dataset(filep,'r')
+        time=postfile.variables['time'][:]
+        n_steps=len(time)
+        if n_steps==nmon:
+          print '%s has the right number of time steps: %s' %(filep,n_steps)
+        else:
+          print 'WARNING: %s has not the correct number of time steps. It has %s and should have %s' %(filep, n_steps,nmon)
+          badfiles=badfiles+1
+          badfilesname.append(filep)
+        
+if badfiles==0:
+  print "CHECKING FINISHED SUCCESSFULLY: NUMBER OF TIME STEPS IN ALL FILES WERE CORRECT"
+else:
+  print "WARNING: CHECKING FINISHED BUT %s FILES HAD INCORRECT NUMBER OF TIME STEPS" %(badfiles)
+  print "THE INCORRECT FILES ARE LOCATED in: \n"
+  print "The incorrect files are: \n"
+  for bdf in badfilesname:
+    print bdf
+        
+
+
