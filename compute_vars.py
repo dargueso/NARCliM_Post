NARCliMpost: I made some changes in compute_vars.py in order to fix potential evaporation units. I added a check_zeros module in postprocess_modules that is called in postprocess_NAR... and checks for fields that only contain zero values aand replace them by missing values...

compute_vars.py
@@ -316,10 +316,12 @@ def compute_sst(varvals,time,gvars):
 def compute_potevp(varvals,time,gvars):
     """Method to compute surface potential evaporation flux
        The original variable was accumulated throughout the simulation. Accumulation must be removed.
-       potevp_in: accumulated surface potential evaporation. Including the timestep previous to the first one in this period [kg m-2]
+       potevp_in: accumulated surface potential evaporation. Including the timestep previous to the first one in this period [W/m-2]\
+       We convert the units from [W/m-2] to [kg m-2 s-1]: multiply the mean rate by the total number of seconds (3*3600) and 
+       divide by the latent heat of vaporization. I assume that the mean temperature is about 20 C.
        time: list of times corresponding to potevp 1st dimension
        ---
-       potevp_out: Surface evaporation flux [kg m-2 s-1]
+       potevp_out: Potential evaporation flux [kg m-2 s-1]
        atts: attributes of the output variable to be used in the output netcdf
     """
     potevp_in=varvals['POTEVP'][:]
@@ -329,12 +331,12 @@ def compute_potevp(varvals,time,gvars):
         sys.exit('ERROR in compute_potevp: The lenght of time variable does not correspond to var first dimension')    
     
     tseconds=round(((time[-1]-time[0]).total_seconds()/len(time)))
-    atts=pm.get_varatt(sn="water_potential_evaporation_flux",ln="Potential evaporation",un="W m-2",ts="time: point values %s seconds" %(tseconds))
+    atts=pm.get_varatt(sn="water_potential_evaporation_flux",ln="Potential evaporation",un="kg m-2 s-1",ts="time: point values %s seconds" %(tseconds))
     
     #Calculating difference between each timestep to remove the accumulation
     #Divided by the number of seconds in each timestep to calculate the flux
     potevp_out=np.zeros((potevp_in.shape[0]-1,)+potevp_in.shape[1:],dtype=np.float64)
-    potevp_out[:,:,:]=np.diff(potevp_in,axis=0)/tseconds
+    potevp_out[:,:,:]=np.diff(potevp_in,axis=0)*tseconds/pm.function_latentheat(293.)
     
     return potevp_out,atts
 

postprocess_modules.py
@@ -31,7 +31,18 @@ class const:
   rcp = Rd/cp
   tkelvin = 273.15
   missingval = 1.e+20
-  
+ 
+
+# *************************************************************************************
+def function_latentheat(T):
+  '''
+  Function to calculate the vaporization latent heat as a function of temperature (equation valide between -25 and 40 C)
+  '''
+  if T>250:
+    T=T-const.tkelvin
+
+  L = 1000.*(2500.8 - 2.36*T + 0.0016*T**2 - 0.00006*T**3) # in J/kg
+  return L
   
 # *************************************************************************************
 class gvar:
@@ -494,6 +505,62 @@ def check_pracc_values(varval,date):
     
   return error_msg
 
+# *************************************************************************************
+def check_zeros_values(varval,date,gvars,filet):
+  """ Check for zeros values in wrfdly and wrfxtrm files. For those time steps where all
+  fields are zero we replace them by missing values.
+  A message error is written in the log file. Also an error message is displayed at 
+  the end of the log file.
+
+  Input: a given variable from wrdly-wrfxtrm files.  
+  Output: error message or nothing
+  Author: Alejanro Di Luca, Daniel Argueso
+  Created: 10/05/2014
+  Last Modification: 10/05/2014
+
+  """
+  print '\n', ' CHECKING FOR FIELDS WITH ALL ZEROS ','\n'
+  import numpy as np
+  import sys
+
+  error_msg=''
+  error_dates=[]
+
+  count=0
+  # Check for zeros in the variable
+  for tstep in range(0,varval.shape[0]):
+    Zero = np.all(varval[tstep,:,:]==0)
+  
+    if Zero==True:    
+      mm=str(date[tstep].month)
+      if date[tstep].month<=9:
+        mm='0'+str(date[tstep].month)
+      
+      # Check for zeros in the a variable that cannot have all zeros
+      filename=gvars.pathin+filet+'_'+gvars.domain+'_'+str(date[tstep].year)+'-'+mm+'-01_00:00:00'
+      fin=nc.Dataset(filename,mode='r')
+      if filet=='wrfxtrm':
+        temp=fin.variables['T2MEAN'][:]
+      if filet=='wrfdly':
+        temp=fin.variables['UV10MAX5'][:]
+      nn=int(date[tstep].day)-1
+      Zero2 = np.all(temp[nn,:,:]==0)
+
+      if Zero2==True:    
+        varval[tstep,:,:]=const.missingval
+        error_dates.append(date[tstep].strftime("%Y-%m-%d %H:%M:%S"))
+        count=count+1
+
+  print "\n", ' ===>>> A TOTAL OF ',count,\
+      '  FIELDS WITH ONLY ZERO VALUES WERE FOUND',\
+      error_dates
+  if count>0:
+    print '     ==>> ALL VALUES WERE SET TO MISSING VALUE '
+    
+  error_msg='THERE ARE ONLY-ZERO VALUES IN:',\
+      error_dates
+    
+  return error_msg
 
 # *************************************************************************************
 def create_netcdf(info,gvars, varval, time, time_bnds):
@@ -878,7 +945,7 @@ def read_list(files_list,var):
   if method=='Dataset':
     varvals={}
 
-    njobs=10
+    njobs=30
     nlen = len(files_list)/njobs #time step block length
     a=len(files_list)-njobs*nlen 
     nt_v=np.zeros(njobs)