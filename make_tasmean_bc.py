044bfae778bf5c10e72215a0d2010569cae71e84
Roman.Olson <roman.olson@unsw.edu.au>
2015-01-29 17:13:13 +1100

    A new program to create bias-corrected tasmean files from tasminmean_bc and tasmaxmean_bc files.
    	new file:   NARCliM_postprocess/make_tasmean_bc.py
    	new file:   NARCliM_postprocess/tests/test_make_tasmean_bc.py

---
+++ make_tasmean_bc.py
@@ -0,0 +1,96 @@
+# ======================================================================
+# PURPOSE
+# =========
+# To calculate tasmean_bc (mean temperature) from tasminmean_bc
+# (mean minimum temperature) and tasmaxmean_bc (mean maximum
+# temperature) files for the NARCliM project and output the result
+# into a similarly formatted NetCDF file. Author and email of the
+# resulting file are specified as inputs.
+
+# STATUS
+# ========
+# Working
+
+# CAUTION! Only expected to be tested on the monthly files.
+
+# NOTE! We change the data type to float64. Regardless of the initial
+# _FillValue, the output data is filled with 1E+20. 
+
+# INPUT
+# =======
+# minfile => NARCliM NetCDF file containing tasminmean_bc. String. 
+# maxfile => NARCliM NetCDF file containing tasmaxmean_bc. String. 
+# outfile => NetCDF file where to output tasmean_bc. String ending with "*.nc".
+# author  => File's author (for global attributes). String.
+# email   => Author's email (for global attributes). String. 
+
+# CALLS
+# =======
+# read_narclim_var and create_netcdf_flexi from netcdf_utils
+
+# OUTPUT
+# ========
+# outfile => Mean temperature file. NARCliM NetCDF4 file. Minor
+# modifications are made to global and variable attributes, which are
+# generally copied from the minimum temperature file. 
+
+# REQUIRES
+# =========
+# Python 2.7.5SL, numpy 1.7.1, netCDF 1.0.4,  netcdf/4.2.1-intel library
+
+# HISTORY
+# ========
+# Jan 29, 2014 => Written by Roman Olson, UNSW.
+# ======================================================================
+def make_tasmean_bc(minfile, maxfile, outfile, author, email):
+
+
+   # SOURCE MODULES
+   from netcdf_utils import create_netcdf_flexi, read_narclim_var
+   import netCDF4 as ncdf
+   from datetime import datetime 
+   import numpy as np
+   import numpy.ma as ma
+   import pdb
+
+   
+   # OPEN MINFILE
+   (nclat1, nclon1, nctime1, tmin) = read_narclim_var(minfile, "tasminmean_bc")
+   (nclat2, nclon2, nctime2, tmax) = read_narclim_var(maxfile, "tasmaxmean_bc")
+
+   # CHECKS
+   if (np.any(nclat1 != nclat2)):
+      raise ValueError, "Latitudes don't match"
+   if (np.any(nclon1 != nclon2)):
+      raise ValueError, "Longitudes don't match"
+   if (np.any(nctime1 != nctime2)):
+      raise ValueError, "Times don't match" 
+
+   # OBTAIN MEAN TEMPERATURE AND CONVERT TO MASKED ARRAY
+   tasmean_reg = (tmin + tmax)/2.0 
+   tasmean_pt = ma.array(tasmean_reg, dtype=np.float64, 
+      mask=np.isnan(tasmean_reg), fill_value=1.0E20)
+
+   # CREATE ATTRIBUTES DICTIONARY
+   # Globals
+   minfileobj              = ncdf.Dataset(minfile)
+   minfile_glob            = minfileobj.__dict__.copy()
+   minfile_glob["author"]  = author
+   minfile_glob["contact"] = email 
+   minfile_glob["history"] = "\n On " + datetime.utcnow().isoformat(' ') + \
+   " UTC created mean temperature from tasminmean_bc and tasmaxmean_bc." 
+
+   # Tasmean
+   tasmean_attrs = dict(minfileobj.variables["tasminmean_bc"].__dict__.copy())
+   del tasmean_attrs["_FillValue"]
+   tasmean_attrs.update(history="Created by averaging tasminmean_bc (monthly\
+ mean minimum temperature) and tasmaxmean_bc (monthly mean maximum temperature)\
+.")
+   attrs = {"tasmean_bc":tasmean_attrs, "global":minfile_glob} 
+
+   # WRITE OUTPUT FILE
+   create_netcdf_flexi(outfile, tasmean_pt, ["lon", "lat", "time"], 
+      "Projection run", varname="tasmean_bc", time=nctime1, lon=nclon1, 
+      lat=nclat1, attr_file=minfile, attrs=attrs)
+
+   
---
+++ tests/test_make_tasmean_bc.py
@@ -0,0 +1,294 @@
+# To test make_tasmean_bc
+# Jan 29, 2014. Roman Olson, CCRC, UNSW.
+
+import pdb
+import unittest
+import numpy as np
+import numpy.ma as ma
+import netCDF4 as ncdf
+from make_tasmean_bc import make_tasmean_bc
+from ccrc_utils import filediff
+import subprocess
+
+class test_make_tasmean_bc(unittest.TestCase):
+   
+   # Set-up. This is done prior to each test.
+   def setUp(self):
+      self.testfiledir = "/home/z3481416/hdrive/work/test_files/bias_corrected/"
+      self.CCCMA_d01_tmin = self.testfiledir + "CCCMA3.1_d01_tasminmean.nc"
+      self.CCCMA_d01_tmax = self.testfiledir + "CCCMA3.1_d01_tasmaxmean.nc"
+      self.CCCMA_2030tmax = self.testfiledir + "CCCMA3.1_d01_2030-2039\
+_tasmaxmean.nc"
+      self.CCCMA_d02_tmax = self.testfiledir + "CCCMA3.1_d02_tasmaxmean.nc"
+      self.NNRP_d02_tmin = self.testfiledir + "NNRP_d02_tasminmean.nc"
+      self.NNRP_d02_tmax = self.testfiledir + "NNRP_d02_tasmaxmean.nc"
+      
+   # Tear-down. This is done after each test
+   def TearDown(self):
+      pass
+      subprocess.call(["rm", "out.nc"])
+      subprocess.call(["rm", "outfile.nc"])
+
+   # Failure cases
+   def test1_except(self):
+      # Different domains
+      self.assertRaisesRegexp(ValueError, "Latitudes don't match", 
+         make_tasmean_bc, self.CCCMA_d01_tmin, self.CCCMA_d02_tmax, "out.nc",
+         "John Smith", "email@email.email")
+      self.assertRaisesRegexp(ValueError, "Times don't match", 
+         make_tasmean_bc, self.CCCMA_d01_tmin, self.CCCMA_2030tmax, "out.nc",
+         "John Smith", "email@email.email")#!+
+      
+
+   # TEST WITH CCCMA3.1 R1 2020-2029 d01
+   def test2(self):
+      make_tasmean_bc(self.CCCMA_d01_tmin, self.CCCMA_d01_tmax, "out.nc", 
+         "John Smith", "email@email.email")
+
+      # Test coordinates
+      ncnew    = ncdf.Dataset("out.nc")
+      ncminold = ncdf.Dataset(self.CCCMA_d01_tmin)
+
+      # Latitudes
+      nclat    = ncnew.variables["lat"]
+      nclatold = ncminold.variables["lat"]
+
+      latdataok  = np.all(nclat[:] == nclatold[:])
+      latshapeok = nclat[:].shape == (144, 215)
+      latdictnew = nclat.__dict__.copy()
+      latdictold = nclatold.__dict__
+      latfillok  = latdictnew["_FillValue"] == 1e+20
+      latdictnew.pop("_FillValue")
+      latdictok = latdictold == latdictnew
+
+      latok = latdataok and latshapeok and latfillok and latdictok
+      self.assertTrue(latok) #!+
+
+      # Longitudes
+      nclon    = ncnew.variables["lon"]
+      nclonold = ncminold.variables["lon"]
+
+      londataok  = np.all(nclon[:] == nclonold[:])
+      lonshapeok = nclon[:].shape == (144, 215)
+      londictnew = nclon.__dict__.copy()
+      londictold = nclonold.__dict__
+      lonfillok  = londictnew["_FillValue"] == 1e+20
+      londictnew.pop("_FillValue")
+      londictok = londictold == londictnew
+
+      lonok = londataok and lonshapeok and lonfillok and londictok
+      self.assertTrue(lonok)
+      
+      # Time
+      nctime    = ncnew.variables["time"]
+      nctimeold = ncminold.variables["time"]
+
+      timedataok  = np.all(nctime[:] == nctimeold[:])
+      timeshapeok = nctime[:].shape == (120,)
+      timedictnew = nctime.__dict__.copy()
+      timedictold = nctimeold.__dict__
+      timefillok  = timedictnew["_FillValue"] == 1e+20
+      timedictnew.pop("_FillValue")
+      timedictold.pop("bounds")
+      timedictok = timedictold == timedictnew
+
+      timeok = timedataok and timeshapeok and timefillok and timedictok
+      self.assertTrue(timeok) 
+
+      # Test data
+      ncdatavar  = ncnew.variables["tasmean_bc"]
+      ncdata     = ncdatavar[:]
+      ncoldtmin  = ncminold.variables["tasminmean_bc"]
+      ncdatamask = ma.getmaskarray(ncdata)
+
+      self.assertTrue(ncdatamask[0,0,0] == True)
+      self.assertTrue(ncdatamask[0, 39, 48] == True)
+      self.assertTrue(ncdata.data[0,0,0] == 1E+20)
+      self.assertTrue(ncdata.data[0, 39, 48] == 1E+20)
+      self.assertAlmostEqual(ncdata[0, 40, 48], 295.214, places=3)
+      self.assertAlmostEqual(ncdata[4, 44, 52], 288.413, places=3)
+      self.assertAlmostEqual(ncdata[119, 101, 95], 302.404, places=3)
+      datafillok  = ncdata.fill_value == 1E+20
+
+      datashapeok = ncdata.shape == (120, 144, 215)
+      datatypeok  = type(ncdata) == np.ma.core.MaskedArray
+      datadtypeok = ncdata.dtype == np.float64
+
+      datadictnew = ncdatavar.__dict__
+      datadictold = ncoldtmin.__dict__
+      dataattrshistok = datadictnew["history"] == "Created by averaging\
+ tasminmean_bc (monthly\
+ mean minimum temperature) and tasmaxmean_bc (monthly mean maximum temperature)\
+."
+      datadictnew.pop("history")
+      # Can't compare fill values since they have changed type
+      self.assertEqual(datadictnew["_FillValue"], 1e+20)
+      datadictnew.pop("_FillValue")
+      datadictold.pop("_FillValue")
+      dataattrsok = dict(datadictnew) == dict(datadictold)
+
+      dataok = datafillok and datashapeok and datatypeok and datadtypeok \
+ and dataattrshistok and dataattrsok
+      self.assertTrue(dataok) 
+
+      
+      # Test global attributes
+      globnew = ncnew.__dict__.copy()
+      globold = ncminold.__dict__.copy()
+
+      histok = str(globnew["history"]).endswith("UTC created mean temperature\
+ from tasminmean_bc and tasmaxmean_bc.")
+      authorok = globnew["author"] == "John Smith"
+      emailok  = globnew["contact"] == "email@email.email"
+
+      globnew.pop("history")
+      globold.pop("history")
+      globnew.pop("author")
+      globold.pop("author")
+      globnew.pop("contact")
+      globold.pop("contact")
+      globnew.pop("creation_date")
+      globold.pop("creation_date")
+
+      # New file has no "Conventions" or "date"
+      globold.pop("Conventions")
+      globold.pop("date")
+
+      globattrsok = dict(globnew) == dict(globold)
+      globok = histok and authorok and emailok and globattrsok
+
+      self.assertTrue(globok) #!+
+     
+
+   # TEST WITH NNRP R3 1950-1959 d02
+   def test3(self):
+      make_tasmean_bc(self.NNRP_d02_tmin, self.NNRP_d02_tmax, "outfile.nc", 
+         "Roman Olson", "privatemail@email.privatemail")
+
+      # Test coordinates
+      ncnew    = ncdf.Dataset("outfile.nc")
+      ncminold = ncdf.Dataset(self.NNRP_d02_tmin)
+
+      # Latitudes
+      nclat    = ncnew.variables["lat"]
+      nclatold = ncminold.variables["lat"]
+
+      latdataok  = np.all(nclat[:] == nclatold[:])
+      latshapeok = nclat[:].shape == (200, 325)
+      latdictnew = nclat.__dict__.copy()
+      latdictold = nclatold.__dict__
+      latfillok  = latdictnew["_FillValue"] == 1e+20
+      latdictnew.pop("_FillValue")
+      latdictok = latdictold == latdictnew
+
+      latok = latdataok and latshapeok and latfillok and latdictok
+      self.assertTrue(latok) 
+
+      # Longitudes
+      nclon    = ncnew.variables["lon"]
+      nclonold = ncminold.variables["lon"]
+
+      londataok  = np.all(nclon[:] == nclonold[:])
+      lonshapeok = nclon[:].shape == (200, 325)
+      londictnew = nclon.__dict__.copy()
+      londictold = nclonold.__dict__
+      lonfillok  = londictnew["_FillValue"] == 1e+20
+      londictnew.pop("_FillValue")
+      londictok = londictold == londictnew
+
+      lonok = londataok and lonshapeok and lonfillok and londictok
+      self.assertTrue(lonok) #!+
+
+
+      
+      # Time
+      nctime    = ncnew.variables["time"]
+      nctimeold = ncminold.variables["time"]
+
+      timedataok  = np.all(nctime[:] == nctimeold[:])
+      timeshapeok = nctime[:].shape == (120,)
+      timedictnew = nctime.__dict__.copy()
+      timedictold = nctimeold.__dict__
+      timefillok  = timedictnew["_FillValue"] == 1e+20
+      timedictnew.pop("_FillValue")
+      timedictold.pop("bounds")
+      timedictok = timedictold == timedictnew
+
+      timeok = timedataok and timeshapeok and timefillok and timedictok
+      self.assertTrue(timeok) #!+
+
+      # Test data
+      ncdatavar  = ncnew.variables["tasmean_bc"]
+      ncdata     = ncdatavar[:]
+      ncoldtmin  = ncminold.variables["tasminmean_bc"]
+      ncdatamask = ma.getmaskarray(ncdata)
+
+      self.assertTrue(ncdatamask[0,0,0] == True)
+      self.assertTrue(ncdatamask[0, 30, 144] == True)
+      self.assertTrue(ncdata.data[0,0,0] == 1E+20)
+      self.assertTrue(ncdata.data[0, 30, 144] == 1E+20)
+      self.assertAlmostEqual(ncdata[0, 31, 144], 290.694, places=3)
+      self.assertAlmostEqual(ncdata[4, 100, 99], 287.235, places=3)
+      self.assertAlmostEqual(ncdata[119, 199, 0], 302.023, places=3)
+
+      # Test for means
+      ncmean = np.mean(ncdata, dtype=np.float64)
+      self.assertAlmostEqual(ncmean, 292.101, places=3) 
+
+      datafillok  = ncdata.fill_value == 1E+20
+
+      datashapeok = ncdata.shape == (120, 200, 325)
+      datatypeok  = type(ncdata) == np.ma.core.MaskedArray
+      datadtypeok = ncdata.dtype == np.float64
+
+      datadictnew = ncdatavar.__dict__
+      datadictold = ncoldtmin.__dict__
+      dataattrshistok = datadictnew["history"] == "Created by averaging\
+ tasminmean_bc (monthly\
+ mean minimum temperature) and tasmaxmean_bc (monthly mean maximum temperature)\
+."
+      datadictnew.pop("history")
+      # Can't compare fill values since they have changed type
+      self.assertEqual(datadictnew["_FillValue"], 1e+20)
+      datadictnew.pop("_FillValue")
+      datadictold.pop("_FillValue")
+      dataattrsok = dict(datadictnew) == dict(datadictold)
+
+      dataok = datafillok and datashapeok and datatypeok and datadtypeok \
+ and dataattrshistok and dataattrsok
+      self.assertTrue(dataok) #!+
+
+      
+      # Test global attributes
+      globnew = ncnew.__dict__.copy()
+      globold = ncminold.__dict__.copy()
+
+      histok = str(globnew["history"]).endswith("UTC created mean temperature\
+ from tasminmean_bc and tasmaxmean_bc.")
+      authorok = globnew["author"] == "Roman Olson"
+      emailok  = globnew["contact"] == "privatemail@email.privatemail"
+
+      globnew.pop("history")
+      globold.pop("history")
+      globnew.pop("author")
+      globold.pop("author")
+      globnew.pop("contact")
+      globold.pop("contact")
+      globnew.pop("creation_date")
+      globold.pop("creation_date")
+
+      # New file has no "Conventions" or "date"
+      globold.pop("Conventions")
+      globold.pop("date")
+
+      globattrsok = dict(globnew) == dict(globold)
+      globok = histok and authorok and emailok and globattrsok
+
+      self.assertTrue(globok) #!+
+
+
+
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(test_make_tasmean_bc)
+unittest.TextTestRunner(verbosity=2).run(suite)
