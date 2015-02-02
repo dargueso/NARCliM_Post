6738026791404aa539007fc9b231dccf08ddc8da
Roman.Olson <roman.olson@unsw.edu.au>
2015-02-02 15:01:49 +1100

    New code to create tasmean_bc files from all tasminmean_bc and tasmaxmean_bc files.
    	modified:   NARCliM_postprocess/make_tasmean_bc_all.py
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R1/1990-2010/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R1/1990-2010/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R1/1990-2010/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R1/1990-2010/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R1/2020-2040/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R1/2020-2040/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R1/2020-2040/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R1/2020-2040/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R1/2060-2080/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R1/2060-2080/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R1/2060-2080/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R1/2060-2080/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R2/1990-2010/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R2/1990-2010/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R2/1990-2010/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R2/1990-2010/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R2/2020-2040/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R2/2020-2040/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R2/2020-2040/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R2/2020-2040/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R2/2060-2080/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R2/2060-2080/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R2/2060-2080/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R2/2060-2080/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R3/1990-2010/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R3/1990-2010/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R3/1990-2010/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R3/1990-2010/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R3/2020-2040/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R3/2020-2040/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R3/2020-2040/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R3/2020-2040/d02/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R3/2060-2080/d01/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R3/2060-2080/d01/.gitkeep
    	renamed:    NARCliM_postprocess/tests/bcor/CSIRO-MK30/R3/2060-2080/d02/.gitkeep -> NARCliM_postprocess/tests/bcor/CSIRO-MK3.0/R3/2060-2080/d02/.gitkeep
    	new file:   NARCliM_postprocess/tests/rmtestfiles
    	modified:   NARCliM_postprocess/tests/test_make_tasmean_bc_all.py

--- make_tasmean_bc_all.py
+++ make_tasmean_bc_all.py
@@ -6,6 +6,9 @@
 
 # STATUS
 # ========
+# Working
+
+# Have not tested the case where outputdir = inputdir
 
 # INPUT
 # =======
@@ -14,11 +17,12 @@
 #               bias-corrected files in
 #               NARCliM format as [GCM]/[RCM]/[period]/[d01/d02]. Example:
 #              NNRP/R2/1950-2010/d01/CCRC_NARCliM_MON_1950-1959_tasminmean_bc.nc
-# outputdur  => Full output directory where to place the
-#               file. String. Can be the
+# outputdir  => Full output directory where to place the
+#               file. String. Must end with slash. Can be the
 #               same as input directory. If different, has to be an
 #               empty directory structure of the same form as the
-#               input directory structure. 
+#               input directory structure. This structure can be
+#               created with script createnarclimdirs.
 # author     => Author for the globals of output files. String. 
 # email      => Author email for the globals of output files. String. 
 
@@ -29,53 +33,57 @@
 # OUTPUT
 # ========
 # tasmean_bc files in the output directory structure. For example:  
-# NNRP/R2/1950-2010/d01/CCRC_NARCliM_MON_1950-1959_tasminmean_bc.nc
+# NNRP/R2/1950-2010/d01/CCRC_NARCliM_MON_1950-1959_tasmmean_bc.nc
 
 # REQUIRES
 # =========
+# Python 2.7.5SL, numpy 1.7.1, netCDF 1.0.4,  netcdf/4.2.1-intel library
 
 # HISTORY
 # ========
-# Jan 30 2015 => Written by Roman Olson, CCRC, UNSW.
+# Feb 02 2015 => Written by Roman Olson, CCRC, UNSW.
 # ======================================================================
 
-def make_tasmean_bc_all(inputdir, outputdir, author, email):
+def make_tasmean_bc_all(inputdir, outputdir, author, email): 
 
    # IMPORT MODULES
    import os
    import glob
-   from make_tasmean_bc import make_tasmean_bc
+   from make_tasmean_bc import make_tasmean_bc 
+   import pdb
 
    # WALK THROUGH DIRECTORIES
    # Note that dirpath does not end with a slash
-   for dirpath, dirnames, filenames in os.walk(inputdir):
-
-      print dirpath
+   for dirpath, dirnames, filenames in os.walk(inputdir): 
 
       # Only look into bottom directories ending in d01 or d02
       if (dirpath.endswith(("d01", "d02"))):
 
         reldirpath = os.path.relpath(dirpath, inputdir)
         print reldirpath
-        minfiles = glob.glob(dirpath + "/CCRC_NARCliM_MON_*_tasminmean_bc.nc")
-        maxfiles = glob.glob(dirpath + "/CCRC_NARCliM_MON_*_tasmaxmean_bc.nc")
-        
-        # Iterate across files for each directory. These are full names
+        # Note that these are full paths
+        minfiles = sorted(glob.glob(dirpath + 
+"/CCRC_NARCliM_MON_*_tasminmean_bc.nc"))
+        maxfiles = sorted(glob.glob(dirpath + 
+"/CCRC_NARCliM_MON_*_tasmaxmean_bc.nc"))
+        if (len(minfiles) != len(maxfiles)):
+           raise ValueError, "Different number of min and max files"
+
+        # Iterate across files for each directory. 
         for minfile, maxfile in zip(minfiles, maxfiles):
 
            baseminfile = os.path.basename(minfile)
-           basemaxfile = os.path.basename(maxfile)
+           basemaxfile = os.path.basename(maxfile) 
 
-           # Check if files are for the same period
+           # Check if files cover the same period
            if (baseminfile[:26] == basemaxfile[:26]):
 
               basemeanfile = baseminfile[:26] + "_tasmean_bc.nc"
               meanfile = os.path.join(outputdir, reldirpath, basemeanfile)
-
               make_tasmean_bc(minfile, maxfile, meanfile, 
                  author, email)
 
-           else:
+           else: 
               raise ValueError, "Min and max files have different times"
 
 
--- tests/test_make_tasmean_bc_all.py
+++ tests/test_make_tasmean_bc_all.py
@@ -1,34 +1,146 @@
 # To test make_tasmean_bc_all
-
+# Feb 02 2014 => Roman Olson, CCRC, UNSW.
+# Program runs during set-up, and the results are compared to
+# individual program calls in the tests. The individual file check is
+# passed no matter what. However, the output goes to the screen and
+# needs to be checked visually by the user. The good result is
+# differences in creation date, etc., which is what to be expected. 
 import pdb
 import unittest
 import numpy as np
 from make_tasmean_bc_all import make_tasmean_bc_all
+from make_tasmean_bc import make_tasmean_bc
 from ccrc_utils import filediff
+import subprocess
+import os.path
 
 class test_make_tasmean_bc_all(unittest.TestCase):
    
    # Set-up. This is done prior to each test.
    def setUp(self):
       self.biasdir = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/"
-      self.outdir = "/home/z3481416/hdrive/work/narclim_scripts_git/\
+      self.outdir = "/srv/ccrc/data30/z3481416/narclim_scripts_git/\
 narclim_scripts/NARCliM_postprocess/tests/bcor/"
-      
-   # Tear-down. This is done after each test
-   def TearDown(self):
-      pass
-
-   # Test run
-   def test_run(self):
       make_tasmean_bc_all(self.biasdir, self.outdir, "Roman Olson", 
-         "roman.olson@unsw.edu.au")
+   "privatemail@email.privatemail")
+
       
+   # Tear-down. This is done after each test. 
+   def tearDown(self):
+      pass
+      print "Removing test files!"
+      subprocess.call(['bash', 'rmtestfiles'])
+      subprocess.call(['rm', 'make_tasmean_bc_all_test1_1.nc'])
+      subprocess.call(['rm', 'make_tasmean_bc_all_test1_2.nc'])
+      subprocess.call(['rm', 'make_tasmean_bc_all_test1_3.nc'])
+
+   # Test1.
+   def test1(self):
+      pass
+      # CHECK TOTAL NUMBER OF FILES AND CHECK THE FILENAMES
+      GCMs = ('MIROC3.2', 'ECHAM5', 'CCCMA3.1', 'CSIRO-MK3.0')
+      RCMs = ('R1', 'R2', 'R3')
+      domains = ('d01', 'd02')
+      periods = ('1990-2010', '2020-2040', '2060-2080')
+      modelfilesok = []
+      rnlsfilesok = []
+
+      # GCM files
+      print "================="
+      print "Testing GCM files"
+      print "================="
+      for GCM in GCMs:
+         for RCM in RCMs:
+            for period in periods:
+               for domain in domains:
+                  curdir = os.path.join("bcor/", GCM, RCM, period, domain, "")
+                  pstart = int(period[:4])
+
+                  file1 = "CCRC_NARCliM_MON_" + str(pstart) + "-" +\
+ str(pstart+9) + "_tasmean_bc.nc"
+                  file2 = "CCRC_NARCliM_MON_" + str(pstart+10) + "-" +\
+ str(pstart+19) + "_tasmean_bc.nc"
+                  fullfile1 = os.path.join(curdir, file1)
+                  fullfile2 = os.path.join(curdir, file2)
+                  print fullfile1
+                  print fullfile2
+                  modelfilesok.append(os.path.isfile(fullfile1))
+                  modelfilesok.append(os.path.isfile(fullfile2))
+      gcmfilesallok = np.all(modelfilesok)
+      gcmnumok = len(modelfilesok) == 144 
+
+      # Reanalysis files
+      rp_start = [1950, 1960, 1970, 1980, 1990, 2000]
+      print "========================"
+      print "Testing reanalysis files"
+      print "========================"
+      for RCM in RCMs:
+         for domain in domains:
+            curdir = os.path.join("bcor/NNRP", RCM, "1950-2010", domain, "")
+
+            for rp in rp_start:
+               myfile = "CCRC_NARCliM_MON_"  + str(rp) + "-" +\
+ str(rp+9) + "_tasmean_bc.nc"
+               myfullfile = os.path.join(curdir, myfile)
+               rnlsfilesok.append(os.path.isfile(myfullfile))
+               print myfullfile
+      rnlsfilesallok = np.all(rnlsfilesok)
+      rnlsnumok = len(rnlsfilesok) == 36 
+
+      filesok = gcmfilesallok and gcmnumok and rnlsfilesallok and rnlsnumok
+      self.assertTrue(filesok)#!+
 
 
+      #Check 1: CCCMA3.1 R1 1990-2000 d01
+      print "========================="
+      print "Differences for CCCMA3.1 R1 1990-2000 d01"
+      print "========================="
+      minfile = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/CCCMA3.1/\
+R1/1990-2010/d01/CCRC_NARCliM_MON_1990-1999_tasminmean_bc.nc"
+      maxfile = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/CCCMA3.1/\
+R1/1990-2010/d01/CCRC_NARCliM_MON_1990-1999_tasmaxmean_bc.nc"
+      outfile = "bcor/CCCMA3.1/R1/1990-2010/d01/CCRC_NARCliM_MON_1990-1999_\
+tasmean_bc.nc"
+      outfile_save = "make_tasmean_bc_all_test1_1.nc"
+      make_tasmean_bc(minfile, maxfile, outfile_save, "Roman Olson", 
+"privatemail@email.privatemail")
+      subprocess.call(["ncdiff", outfile, outfile_save])#!+
+
+      #Check 2: CSIRO-MK3.0 R2 2070-2080 d02
+      print "========================="
+      print "Differences for CSIRO-MK3.0 R2 2070-2080 d02"
+      print "========================="
+      minfile = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/CSIRO-MK3.0/\
+R2/2060-2080/d02/CCRC_NARCliM_MON_2070-2079_tasminmean_bc.nc"
+      maxfile = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/CSIRO-MK3.0/\
+R2/2060-2080/d02/CCRC_NARCliM_MON_2070-2079_tasmaxmean_bc.nc"
+      outfile = "bcor/CSIRO-MK3.0/R2/2060-2080/d02/CCRC_NARCliM_MON_2070-2079_\
+tasmean_bc.nc"
+      outfile_save = "make_tasmean_bc_all_test1_2.nc"
+      make_tasmean_bc(minfile, maxfile, outfile_save, "Roman Olson", 
+"privatemail@email.privatemail")
+      subprocess.call(["ncdiff", outfile, outfile_save])#!+
 
 
+      #Check 3: NNRP R3 1970-1980 d02
+      print "========================="
+      print "Differences for NNRP R3 1970-1980 d02"
+      print "========================="
+      minfile = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/NNRP/\
+R3/1950-2010/d02/CCRC_NARCliM_MON_1970-1979_tasminmean_bc.nc"
+      maxfile = "/srv/ccrc/data30/z3393020/NARCliM/Bias_corrected/NNRP/\
+R3/1950-2010/d02/CCRC_NARCliM_MON_1970-1979_tasmaxmean_bc.nc"
+      outfile = "bcor/NNRP/R3/1950-2010/d02/CCRC_NARCliM_MON_1970-1979_\
+tasmean_bc.nc"
+      outfile_save = "make_tasmean_bc_all_test1_3.nc"
+      make_tasmean_bc(minfile, maxfile, outfile_save, "Roman Olson", 
+"privatemail@email.privatemail")
+      subprocess.call(["ncdiff", outfile, outfile_save])#!+
 
-
-
+   # # Test run
+   # def test_run(self):
+   #    make_tasmean_bc_all(self.biasdir, self.outdir, "Roman Olson", 
+   #       "roman.olson@unsw.edu.au")
+      
 suite = unittest.TestLoader().loadTestsFromTestCase(test_make_tasmean_bc_all)
 unittest.TextTestRunner(verbosity=2).run(suite)
